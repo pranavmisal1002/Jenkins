aab9972b81028c2a07366014169116ae39514044
Pranav Misal <pranavmisal1002@gmail.com>
2026-02-23 17:13:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+provider "aws" {
+    region = "eu-north-1"
+}
+
 resource "aws_iam_role" "cluster-role" {
   name = "eks-cluster-role"
   assume_role_policy = jsonencode({
